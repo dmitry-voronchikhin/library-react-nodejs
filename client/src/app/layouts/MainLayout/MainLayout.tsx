ff671d241f9b7100fb246476bdfeda8273b6541b
dmitry-voronchihin <vdima775@gmail.com>
2022-11-29 16:58:55 +0400

    perf(client->MainLayout): add menu items memoization

--- client/src/app/layouts/MainLayout/MainLayout.tsx
+++ client/src/app/layouts/MainLayout/MainLayout.tsx
@@ -1,4 +1,4 @@
-import React, { FC, ReactNode, useContext } from 'react';
+import React, { FC, ReactNode, useContext, useMemo } from 'react';
 import { Button, Layout, Menu } from 'antd';
 import { Content, Header } from 'antd/lib/layout/layout';
 import { observer } from 'mobx-react-lite';
@@ -15,16 +15,26 @@ import styles from './styles.module.scss';
 const MainLayoutComponent: FC<{ children: ReactNode }> = ({ children }) => {
   const { store } = useContext(Context);
 
-  const menuItems: ItemType[] = compact<ItemType>(
-    routes.map((route) => {
-      if (route.inNav) {
-        return {
-          key: route.name,
-          label: <Link to={route.path}>{route.name}</Link>,
-        };
-      }
-    }),
+  const menuItems: ItemType[] = useMemo(
+    () =>
+      compact<ItemType>(
+        routes.map((route) => {
+          if (route.inNav) {
+            return {
+              key: route.name,
+              label: <Link to={route.path}>{route.name}</Link>,
+            };
+          }
+        }),
+      ),
+    [],
   );
+
+  const defaultSelectedKeys = [menuItems[0]?.key?.toString() || ''];
+
+  const logout = () => {
+    store.logout();
+  };
 
   return (
     <Layout>
@@ -32,18 +42,15 @@ const MainLayoutComponent: FC<{ children: ReactNode }> = ({ children }) => {
         <Menu
           theme="dark"
           mode="horizontal"
-          defaultSelectedKeys={['1']}
+          defaultSelectedKeys={defaultSelectedKeys}
           items={menuItems}
         />
-        <Button onClick={() => store.logout()} type="primary">
+        <Button onClick={logout} type="primary">
           Выйти
         </Button>
       </Header>
       <Content className={cn('site-layout', styles.Content)}>
-        <div
-          className="site-layout-background"
-          style={{ padding: 24, minHeight: 380 }}
-        >
+        <div className={cn('site-layout-background', styles.Content__Body)}>
           {children}
         </div>
       </Content>
